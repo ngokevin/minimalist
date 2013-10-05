26d87dd303e19e6e914fb8433b3ccf373a99508c
Kevin Ngo <ngoke@mozilla.com>
2013-10-05 03:20:18 -0700

    can add

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,17 @@
 angular.module('MinimalistApp')
 
 
-.controller('MainCtrl', ['$scope', function($scope) {
+.controller('MainCtrl', ['$scope', 'EntryService',
+                         function($scope, EntryService) {
 
+    $scope.listNames = EntryService.getListNames();
+    $scope.lists= EntryService.getLists();
+
+    $scope.listName = EntryService.getLastViewedList();
+    $scope.list = EntryService.get($scope.listName);
+
+    $scope.addEntry = function() {
+        EntryService.add($scope.listName, $scope.entry);
+        $scope.entry = '';
+    };
 }]);
--- js/services.js
+++ js/services.js
@@ -6,30 +6,48 @@ angular.module('MinimalistApp', [])
     var loads = JSON.parse;
 
     var storage = {
-        lastViewedList: 0,
-        listNames: [],
-        lists: {}
+        lastViewedList: 'First List',
+        listNames: ['First List'],
+        lists: {'First List': []}
     };
+
     if (localStorage.getItem('storage')) {
+        // Load localStorage into context if exists.
         storage = loads(localStorage.getItem('storage'));
     } else {
+        // Initialize localStorage if doesn't exist.
         localStorage.setItem('storage', dumps(storage));
     }
 
+    var lists = storage.lists;
+
     return {
         get: function(listName) {
-            return storage[listName];
+            return lists[listName];
         },
         add: function(listName, entry) {
-            storage[listName].push(entry);
+            if (!(listName in lists)) {
+                lists[listName] = [];
+                storage.listNames.push(listName);
+            }
+            lists[listName].push(entry);
             localStorage.setItem('storage', dumps(storage));
-            return storage[listName];
+            return lists[listName];
         },
         del: function(listName, item) {
-            i = storage[listName].indexOf(item);
-            storage[listName].splice(i, 1);
+            i = lists[listName].indexOf(item);
+            lists[listName].splice(i, 1);
             localStorage.setItem('storage', dumps(storage));
-            return storage[listName];
+            return lists[listName];
+        },
+        getLastViewedList: function() {
+            return storage.lastViewedList;
+        },
+        getListNames: function() {
+            return storage.listNames;
+        },
+        getLists: function() {
+            return storage.lists;
         }
     };
 });
