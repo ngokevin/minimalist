1e9552719ef3cc8e09591a341e22b55750a9adfc
Kevin Ngo <ngoke@mozilla.com>
2014-01-18 16:43:42 -0800

    work on e2e tests

--- tests/e2e_tests/minimalist_spec.js
+++ tests/e2e_tests/minimalist_spec.js
@@ -4,12 +4,16 @@ describe('minimalist', function() {
     var $submit;
 
     beforeEach(function() {
-        browser.get('http://ngokevin.com/~ngoke/minimalist-dev');
+        browser.get('http://minimalist.local');
         // element = namespace. all = grouping. by = selector.
-        // TODO: element($) doesn't work very well.
-        $items = element.all(by.css('.item'));
+        // NOTE: element($) doesn't work very well for me.
+        $items = element.all(by.css('.item p'));
         $textarea = element(by.css('#add-item-text'));
         $submit = element(by.css('#add-item'));
+    });
+
+    afterEach(function() {
+        browser.executeScript('localStorage.clear();');
     });
 
     it('has an initial list item', function() {
@@ -17,32 +21,61 @@ describe('minimalist', function() {
     });
 
     it('add items', function() {
-        addItem();
+        for (var i = 1; i <= 3; i++) {
+            addItem('lol' + i);
+        }
         setTimeout(function() {
+            expect($items.count()).toEqual(4);
+            expect($items.get(1).getText()).toEqual('lol1');
+            expect($items.get(3).getText()).toEqual('lol3');
+        });
+    });
+
+    it('delete items', function() {
+        addItem('lol');
+        addItem('lol');
+        delItem(0);
+        setTimeout(function() {
+            // initial 1 + add 2 + del 1 = 2 items left.
             expect($items.count()).toEqual(2);
         });
     });
 
-    function addItem() {
-        $textarea.sendKeys('lol');
+    it('add a list', function() {
+        addList('lol list');
+        setTimeout(function() {
+            expect($$('.list-switcher .list-switch').count()).toEqual(2);
+        });
+    });
+
+    function $$(selector) {
+        // Shortcut for element group selector.
+        // NOTE: can't click a whole group.
+        return element.all(by.css(selector));
+    }
+
+    function $(selector) {
+        // Shortcut for element single selector.
+        return element(by.css(selector));
+    }
+
+    function addItem(text) {
+        $textarea.sendKeys(text);
         $submit.click();
     }
 
-    /*
-    it('should list todos', function() {
-        expect(todoList.count()).toEqual(2);
-        expect(todoList.get(1).getText()).toEqual('build an angular app');
-    });
+    function delItem(n) {
+        // Click the delete button of the nth item of the current list.
+        var $deletes = $$('.list .item .delete');
+        $deletes.get(n).click();
+    }
 
-    it('should add a todo', function() {
-        var addTodo = element(by.model('todoText'));
-        var addButton = element(by.css('[value="add"]'));
-
-        addTodo.sendKeys('write a protractor test');
-        addButton.click();
-
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
-    */
+    function addList(listName) {
+        // NOTE: "Element is not currently visible and so may not be interacted with."
+        // Must click around first to get the stuff to show.
+        $('.toggle-lists').click();
+        $('.list-switcher .new').click();
+        $('.new-list input').sendKeys(listName);
+        $('.submit-new-list').click();
+    }
 });
